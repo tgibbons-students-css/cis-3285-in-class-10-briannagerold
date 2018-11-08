8b2d06a1479cd2dd17ae115383c3d3e5a6128382
bgerold <bgerold@LABSEC11-2YVN>
2018-11-08 10:45:09 -0600

    Add a Decorator that converts "GBP" to "EUR"

---
+++ CurrencyTrader/AdjustTradeDataProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyTrader.Contracts;
+using CurrencyTrader;
+
+namespace CurrencyTrader
+{
+    public class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        UrlTradeDataProvider urlTDP;
+        public AdjustTradeDataProvider(String url)
+        {
+            urlTDP = new UrlTradeDataProvider(url);
+        }
+        
+
+        public IEnumerable<string> GetTradeData()
+        {
+            IEnumerable<string> tradeData = urlTDP.GetTradeData();
+            List<string> newTradeData = new List<string>();
+            foreach (string trade in tradeData)
+            {
+                String newTrade;
+                if (trade.Contains("GBP"))
+                {
+                    newTrade = trade.Replace("GBP", "EUR");
+                }
+                else
+                {
+                    newTrade = trade;
+                }
+                newTradeData.Add(newTrade);
+            }
+
+            return newTradeData;
+        }
+    }
+}
